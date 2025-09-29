483bcbe5d156c9ddf348a6d095e7bb506aeb3703
kaizencycle <michaeljjudan@gmail.com>
2025-09-29 12:25:38 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App'; // <-- CHANGED: Removed the .jsx extension
 import './index.css';
 
 // This is where React starts running your App component!
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<React.StrictMode>
-<App />
-</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
