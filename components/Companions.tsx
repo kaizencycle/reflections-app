5d0d649808b717ac581f97f98d1e8c8ce9338ebc
kaizencycle <michaeljjudan@gmail.com>
2025-10-01 12:42:19 -0400

    Create Companions.tsx

---
+++ components/Companions.tsx
@@ -0,0 +1,8 @@
+// components/Companions.tsx
+export default function Companions() {
+  return (
+    <div className="p-4 space-y-3">
+      {/* companion cards: Jade, Hermes, Eve, Zeus */}
+    </div>
+  );
+}
